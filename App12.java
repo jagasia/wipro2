1ff9d86e60d29c41c163d175f9f6d6a588844bd3
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-04-10 12:43:47 +0530

    10Apr 12:43

---
+++ App12.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class App12
+{
+	
+	public static void main(String args[])
+	{
+		HashMap<Integer, String> map=new HashMap<>();
+		map.put(null,"Japan");
+		map.put(null,"China");
+		System.out.println(map);
+	}
+}
