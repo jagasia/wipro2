9e6329679f94d12c708a0ee6259c1d31921e3654
Jagdeeswaran <t-Jagdeeswaran@cognizant.com>
2024-04-16 10:30:31 +0530

    16Apr 10:30

---
+++ Java/16-apr-strings/src/App.java
@@ -0,0 +1,16 @@
+
+public class App {
+
+	public static void main(String[] args) {
+//		String str="hello";
+//		str="world";	//we have jumped to a new location
+//		System.out.println(str);
+		String str="Mumbai is called as Bombay";
+		//can we change "is" into "was"?
+
+//		str=str.replaceAll("is", "was");
+		str="Mumbai was called as Bombay";
+		System.out.println(str);
+	}
+
+}
